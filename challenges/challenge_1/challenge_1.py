f65c185034807998e8c99709c1b07fee523a279d
Jayce Azua <jayce.azua@gmail.com>
2019-07-23 23:58:40 -0700

    [wip]

--- challenges/challenge_1/challenge_1.py
+++ challenges/challenge_1/challenge_1.py
@@ -1,28 +1,14 @@
+import sys
 # vertex node
 
-class Graph():
-  # creates a new, empty graph.
 
-  def addVertex(self, vert):
-    # adds an instance of Vertex to the graph.
-    pass
 
-  def addEdge(self, fromVert, toVert):
-    # Adds a new, directed edge to the graph that connects two vertices.
-    pass
 
-  def addEdgeWithWeight(self, fromVert, toVert, weight):
-    # Adds a new, weighted, directed edge to the graph that connects two vertices.
-    pass
-
-  def getVertex(self, vertKey):
-    # finds the vertex in the graph named vertKey.
-    pass
-
-  def getVertices(self, ):
-    # returns the list of all vertices in the graph.
-    pass
-
-  def getNeighbors(self, x):
-    # lists all vertices y such that there is an edge from the vertex x to the vertex y;
-    pass
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  file = args[0]
+  # g = Graph(file)
+  print("Vertices:")
+  print("Edges:")
+  print("Edge List:")
+  
---
+++ challenges/challenge_1/graph.py
@@ -0,0 +1,69 @@
+class Vertex(object):
+
+    def __init__(self, vertex):
+        self.id = vertex
+        self.neighbors = {}
+
+    def __str__(self):
+        return f'{self.id} adjacent to {[x.id for x in self.neighbors]}'
+
+    def addNeighbor(self, vertex, weight=0):
+        self.neighbors[vertex] = weight
+
+    def getNeighbors(self):
+        return self.neighbors.keys()
+
+    def getId(self):
+        return self.id
+
+    def getEdgeWeight(self, vertex):
+        return self.neighbors[vertex]
+
+    def getEdges(self):
+        return len(self.neighbors.keys())
+
+
+class Graph:
+    # creates a new, empty graph.
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def addVertex(self, vert):
+        self.num_vertices += 1
+        # create a new vertex
+        new_vertex = Vertex(vert)
+        # add the new vertex to the vertex dictionry
+        self.vert_dict[vert] = new_vertex
+        # return the new vertex
+        return new_vertex
+
+    def addEdgeWithWeight(self, fromVert, toVert, weight):
+        """Adds a new, weighted, directed edge to the graph that connects two vertices."""
+           if fromVert not in self.vert_dict or toVert not in self.vert_dict:
+                # add it - or return an error (choice is up to you).
+                raise ValueError("something went wrong...")
+            else:
+                self.vert_dict[fromVert].add_neighbor(self.vert_dict[toVert], weight)
+
+
+    def getVertex(self, vertKey):
+        """finds the vertex in the graph named vertKey."""
+        if vertKey in self.vert_dict.keys():
+            return vertKey
+        else:
+            raise ValueError("Vertex not found!")
+
+    def getVertices(self):
+        """returns the list of all vertices in the graph."""
+        return self.vert_dict.keys()
+
+
+
+# Attribution:
+# Pair Programming session and study session with:
+# 1. Nathan
+# 2. Jake 
