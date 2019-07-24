4f16d784c9b566c0c054e6fc06cb46caa655ace5
Jayce Azua <jayce.azua@gmail.com>
2019-07-24 01:10:07 -0700

    [wip]

--- challenges/challenge_1/challenge_1.py
+++ challenges/challenge_1/challenge_1.py
@@ -2,15 +2,9 @@ import sys
 from graph import Vertex, Graph
 
 
-
-
-
-
 if __name__ == "__main__":
-  args = sys.argv[1:]
-  file = args[0]
-  # g = Graph(file)
-  print("Vertices:")
-  print("Edges:")
-  print("Edge List:")
-  
+    args = sys.argv[1:]
+    file = args[0]
+    print("Vertices:")
+    print("Edges:")
+    print("Edge List:")
