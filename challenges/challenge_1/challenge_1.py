aca6c161180c002375d1d05d7f6eac11e2e126c7
Jayce Azua <jayce.azua@gmail.com>
2019-07-23 20:14:55 -0700

    [wip] completing chalenges

--- challenges/challenge_1/challenge_1.py
+++ challenges/challenge_1/challenge_1.py
@@ -1,21 +1,28 @@
+# vertex node
+
 class Graph():
   # creates a new, empty graph.
 
   def addVertex(self, vert):
-    pass  # adds an instance of Vertex to the graph.
+    # adds an instance of Vertex to the graph.
+    pass
 
   def addEdge(self, fromVert, toVert):
-    pass  # Adds a new, directed edge to the graph that connects two vertices.
+    # Adds a new, directed edge to the graph that connects two vertices.
+    pass
 
   def addEdgeWithWeight(self, fromVert, toVert, weight):
     # Adds a new, weighted, directed edge to the graph that connects two vertices.
     pass
 
   def getVertex(self, vertKey):
-    pass  # finds the vertex in the graph named vertKey.
+    # finds the vertex in the graph named vertKey.
+    pass
 
   def getVertices(self, ):
-    pass  # returns the list of all vertices in the graph.
+    # returns the list of all vertices in the graph.
+    pass
 
   def getNeighbors(self, x):
-    pass  # lists all vertices y such that there is an edge from the vertex x to the vertex y;
+    # lists all vertices y such that there is an edge from the vertex x to the vertex y;
+    pass
